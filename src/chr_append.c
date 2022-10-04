55945a3c1997f74f3ebb9e38894835e89bf1694c
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-04 15:08:35 -0400

    Fixed index error

--- src/chr_append.c
+++ src/chr_append.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 14:47:53 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/04 15:02:25 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/04 15:08:28 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 char	*chr_append(int c, char *str)
 {
 	char	*rtn;
+	int		i;
 
 	if (str == NULL)
 		return (NULL);
 	rtn = ft_calloc((ft_strlen(str) + 2), sizeof(char));
-	if (str[0])
-		ft_strlcpy(rtn, str, ft_strlen(str));
-	rtn[ft_strlen(str)] = c;
+	i = -1;
+	while (str[++i])
+		rtn[i] = str[i];
+	rtn[i] = c;
 	free(str);
 	return (rtn);
 }
